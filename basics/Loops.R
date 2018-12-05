c3dd3e2938b6dd5afc94d5183bd22da9bbfe18a7
VinodHub <42525091+VinodHub@users.noreply.github.com>
2018-12-05 09:44:57 +0530

    Add files via upload

---
+++ basics/Loops.R
@@ -0,0 +1,154 @@
+#3 types of loops
+#repeat
+#While 
+#For
+
+##################If statement##################
+#syntax
+#if (test_expression) {
+#  statement
+#}
+
+x <- 5
+if(x > 0){
+  print("Positive number")
+}
+
+##################If else statement#################
+#Syntax
+#if (test_expression) 
+#   { statement1 }
+#    else 
+#  { statement2 }
+
+x <- 5
+if(x > 0){
+  print("Non-negative number")
+} else {
+  print("Negative number")
+}
+
+if(x > 0) print("Non-negative number") else print("Negative number")
+
+
+#############################If else ladder###########
+#Syntax
+#if ( test_expression1) {
+#  statement1
+#} else if ( test_expression2) {
+#  statement2
+#} else if ( test_expression3) {
+#  statement3
+#} else {
+#  statement4
+#}
+
+x <- 0
+if (x < 0) {
+  print("Negative number")
+} else if (x > 0) {
+  print("Positive number")
+} else
+  print("Zero")
+################################Repeat Loop##########################################
+#Syntax:
+# repeat { 
+#   commands 
+#   if(condition) {
+#     break
+#   }
+# }
+
+v <- c("Hello","loop")
+cnt <- 2
+
+repeat {
+  print(v)
+  cnt <- cnt+1
+  #print(cnt)
+  if(cnt > 5) {
+    break
+  }
+}
+#break is used to stop the iteration and control flow will jump outside the loop
+
+######################################While Loop#############################################
+
+#Syntax: 
+#while (test_expression) {
+#  statement
+#}
+
+v <- c("Hello","while loop")
+cnt <- 2
+
+while (cnt < 7) {
+  print(cnt)
+  print(v)
+  cnt = cnt + 1
+}
+
+
+####################################for loop##################################################
+
+#Syntax:
+#  for (value in vector) {
+#  statements
+#}
+
+i <- 1
+for (j in 1:3)
+{
+  i <- i+1
+  print(i)
+}
+
+x<- c(2,4,6,8)
+for (n in x)
+{
+  print(n)
+  print(n^2)
+}
+
+x<- c("a","b","c")
+for (n in x)
+{
+  print(n)
+}
+
+for (i in seq(1,10,2))
+{
+  print(i)
+}
+
+x<- 1:5 
+for (val in x){
+  print(val)
+  if (val==3)
+  {break}
+}
+
+x<- 1:5 
+for (val in x){
+  if (val==3)
+  {break}
+  print(val)
+}
+##########################################Next################################################
+
+#When we want to skip a certain iteration without coming out of the loop
+#Syntax: next
+for (i in 1:5)
+{
+  if (i==2){next}
+  print(i)
+}
+
+v <- LETTERS[1:6]
+for ( i in v) {
+  
+  if (i == "D") {
+    next
+  }
+  print(i)
+}
---
+++ basics/R_operators.R
@@ -0,0 +1,64 @@
+x <- c(1,2,3,4,5)
+x_1 <- c("a","b","c","d")
+x <- c(2,5,11,12)
+
+x <- 2
+min(x)
+max(x)
+
+#Arithmetic operator
+saurav <- 5+7
+23/5
+23%%5
+3^2
+
+
+#Relational operator
+5>7
+5<7
+10==5
+10==10
+
+
+#Logical operator
+5 ==(10/2)
+a <- TRUE
+b <- TRUE
+a && b
+
+#Assignment operation
+abc <- 10   #Arrow assignment operator
+abc = 20
+
+
+#Vector
+abc <- c(1,2,3,4,5)
+c(1,2,3,4,5)
+c("abc","bcd")
+"Hello"
+
+abc <- 1
+bdc <- c("a","b","c","d")
+def <- c(TRUE,FALSE,TRUE)
+
+class(abc)
+class(bdc)
+class(def)
+
+#Matrix
+#two dimensional
+#Same data type
+
+matrix_1 <- matrix(1:6,nrow=2)
+matrix_1 <- matrix(1:6,nrow=2,byrow = TRUE)
+rbind()
+cbind()
+rm(matrix_1)
+
+abc <- 20
+
+
+
+
+
+
